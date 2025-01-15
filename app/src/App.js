826b4406d8d4f3097e29e9522910054ef6e95a43
Zero <156836802+XZERON0@users.noreply.github.com>
2025-01-16 01:38:54 +0500

    создание регистрации и так же создание крепкого основа

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Main from "./components/Main";
-import Index from "./components/Index";
-import NotFound from "./components/NotFound";
-
+import Main from "./pages/Main";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import { AuthProvider } from "./context/AuthContext";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Главная</Link></li>
-          <li><Link to="/index">Индекс</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/index" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Главная</Link></li>
+            <li><Link to="/index">Индекс</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
